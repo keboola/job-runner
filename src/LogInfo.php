d1b9415f7ac1177af8bb2a1c669515828257ae9e
odin <odinuv@gmail.com>
2021-01-13 13:33:50 +0100

    log job properties with job messages https://keboola.atlassian.net/browse/PS-1679

---
+++ src/LogInfo.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Keboola\ErrorControl\Monolog\LogInfoInterface;
+
+class LogInfo implements LogInfoInterface
+{
+    /**
+     * @var string
+     */
+    private $projectId;
+
+    /**
+     * @var string
+     */
+    private $runId;
+
+    /**
+     * @var string
+     */
+    private $componentId;
+
+    public function __construct(
+        string $runId,
+        string $componentId,
+        string $projectId
+    ) {
+        $this->runId = $runId;
+        $this->componentId = $componentId;
+        $this->projectId = $projectId;
+    }
+
+    public function getProjectId(): string
+    {
+        return $this->projectId;
+    }
+
+    public function getRunId(): string
+    {
+        return $this->runId;
+    }
+
+    public function getComponentId(): string
+    {
+        return $this->componentId;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'component' => $this->getComponentId(),
+            'runId' => $this->getRunId(),
+            'owner' => [
+                'id' => $this->getProjectId(),
+            ],
+        ];
+    }
+}
