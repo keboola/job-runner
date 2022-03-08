ac83d0680de14be1014c1f72eda12dfd87ae890e
Ondrej Popelka <odinuv@gmail.com>
2022-03-08 21:18:31 +0100

    log events for branch jobs
    https://keboola.atlassian.net/browse/PS-3155

---
+++ tests/StorageApiFactoryTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests;
+
+use App\StorageApiFactory;
+use Keboola\StorageApi\BranchAwareClient;
+use Keboola\StorageApi\Client;
+use PHPUnit\Framework\TestCase;
+
+class StorageApiFactoryTest extends TestCase
+{
+    public function testClient(): void
+    {
+        $factory = new StorageApiFactory((string) getenv('STORAGE_API_URL'));
+        $client = $factory->getClient([]);
+        self::assertInstanceOf(Client::class, $client);
+
+        $client = $factory->getBranchClient('1234', []);
+        self::assertInstanceOf(BranchAwareClient::class, $client);
+    }
+}
